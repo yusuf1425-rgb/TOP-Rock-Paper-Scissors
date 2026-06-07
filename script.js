42b9599b3b5c72bf7d52163dcb8a404480ba3825
Yusuf <236015542+yusuf1425-rgb@users.noreply.github.com>
2026-06-07 12:05:31 +0530

    Add logic to play the entire game

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 console.log("Hello World!");
 
-let humanScore, computerScore = 0;
-
 function getComputerChoice() { 
     let randomNum = Math.floor(Math.random() * 3);
     return getRockPaperScissors(randomNum);
@@ -23,26 +21,40 @@ function getHumanChoice(){
     return humanChoice;
 }
 
-function playRound(humanChoice, computerChoice){
-    humanChoice = humanChoice.toLowerCase();
-    if (humanChoice === computerChoice) {
-        console.log("It's a tie!");
-    } else if ((humanChoice === "rock" && computerChoice == "scissors") || 
-        (humanChoice === "paper" && computerChoice === "rock") || 
-        (humanChoice === "scissors" && computerChoice == "paper")){
-        humanScore++;
-        console.log(`You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}`);
-    } else {
-        computerScore ++;
-        console.log(`You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}`);
-    }    
-}
-
 function capitalize(text){
         return text.charAt(0).toUpperCase() + text.slice(1);
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playGame() {
+    let humanScore = 0; 
+    let computerScore = 0;
 
-playRound(humanSelection, computerSelection);
+    function playRound(humanChoice, computerChoice){
+        humanChoice = humanChoice.toLowerCase();
+        if (humanChoice === computerChoice) {
+            console.log("It's a tie!");
+        } else if ((humanChoice === "rock" && computerChoice == "scissors") || 
+            (humanChoice === "paper" && computerChoice === "rock") || 
+            (humanChoice === "scissors" && computerChoice == "paper")){
+            humanScore++;
+            console.log(`You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}`);
+        } else {
+            computerScore ++;
+            console.log(`You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}`);
+        }    
+    }
+
+    for(let i = 0; i < 5; i++){
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    let scoreCard = `\nYour Score: ${humanScore} \nComputer Score: ${computerScore}`
+
+    if(humanScore > computerScore){
+        alert("You win!" + scoreCard)
+    } else {
+        alert("You lose!" + scoreCard)
+    }
+}
+
+playGame();
